899fcc5c197c096ee88fa8a3342b57df41a6050a
kasparkivi <kasparkivi12@gmail.com>
2020-09-15 11:45:49 +0300

    tee

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $books = App\Models\Book::where('language', 'Estonian')->get();
+     
+    return view('welcome',['books'=>$books]);
+
+    
 });
