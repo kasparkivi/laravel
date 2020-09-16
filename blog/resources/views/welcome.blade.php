dd41b1852434b4852e673e76c677a6d497477936
Kaspar Kivi <44016771+kasparkivi@users.noreply.github.com>
2020-09-16 13:02:20 +0300

    Update welcome.blade.php

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -2,40 +2,72 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library</title>
+
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Laravel</title>
+
 </head>
 
-<body>
+    <body>
 
-    <table class="w-full table-auto">
-    <thead class="font-bold">
-        <tr>
-            
-            <td>Pealkiri</td>
-            <td>Autor</td>
-            <td>Aasta</td>
-            <td>Lehekülgi</td>
-            <td>Hind</td>
-            <td>Laoseis</td>
-            <td>Tüüp</td>
-                
-        </tr>
-    </thead>
-    <tbody class="text-sm">
-        @foreach($books as $book)
+        <table class="table-auto w-full">
     
-        <tr>
-  
-            <td>{{ $book->title}}</td>
-            <td></td>
-            <td> {{$book->release_date}}</td>
-            <td> {{$book->pages}}</td>
-            <td> {{$book->price}}</td>
-            <td> {{$book->stock_saldo}}</td>
-            <td> {{$book->type}}</td
-  
-    </tr>
-    @endforeach
+            <thead>
+    
+                <tr> 
+
+                    <td class="px-4 py-2" class="px-4 py-2">Pealkiri</td>
+
+                    <td class="px-4 py-2">Aasta</td>
+
+                    <td class="px-4 py-2">Keel</td>
+
+                    <td class="px-4 py-2">Hind</td>
+
+                    <td class="px-4 py-2">Laoseis</td>
+
+                    <td class="px-4 py-2">Lehekülgi</td>
+
+                    <td class="px-4 py-2">Tüüp</td>
+
+                </tr>
+            
+            </thead>
+            <tbody>
+            
+            @foreach($books as $book)
+
+            <tr>
+            
+                    <td class="border px-4 py-2">{{ $book->title }}</td>
+
+                    <td class="border px-4 py-2">{{ $book->release_date }}</td>
+
+                    <td class="border px-4 py-2">{{ $book->language }}</td>
+
+                    <td class="border px-4 py-2">{{ $book->price }}</td>
+
+                    <td class="border px-4 py-2">{{ $book->stock_saldo }}</td>
+
+                    <td class="border px-4 py-2">{{ $book->pages }}</td>
+
+                    <td class="border px-4 py-2">{{ $book->type }}</td>
+            </tr>
+
+
+
+            @endforeach
+            
+            </tbody>
+
+        </table>
+
+    </body>
+
+</html>
+
+
 
